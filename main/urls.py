dd6b4e6644d1b719fd6b79b7755a8ee73d2652d1
UnGerardo <gerardojdls@gmail.com>
2022-01-12 08:11:03 -0800

    Added route that takes in an int as an argument, uses it as id to search for item in todolist

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from main.views import landingPage
 
 urlpatterns = [
-    path('', landingPage, name='LandingPage'),
+    path('<int:id>/', landingPage, name='LandingPage'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
-def landingPage(request):
-    return HttpResponse("<h1>Landing Page</h1>")
+def landingPage(request, id):
+    try:
+        return HttpResponse(f"<h1>{models.ToDoList.objects.get(id=1).item_set.get(id=id)}</h1>")
+    except:
+        return HttpResponse(f"<h1>An item with id: {id} doesn't exist.</h1>")
