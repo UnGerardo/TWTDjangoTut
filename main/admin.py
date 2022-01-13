431aed81c7d02575836f626531eee6bf1239349b
UnGerardo <gerardojdls@gmail.com>
2022-01-13 13:44:02 -0800

    Registered models in admin.py to have them visible on the admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ToDoList, Item
 
-# Register your models here.
+# Register your models here. To see on admin page
+admin.site.register(ToDoList)
+admin.site.register(Item)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import models
+from .models import ToDoList, Item
 
 # Create your views here.
 def landingPage(request, id):
     try:
-        return HttpResponse(f"<h1>{models.ToDoList.objects.get(id=1).item_set.get(id=id)}</h1>")
+        return HttpResponse(f"<h1>{ToDoList.objects.get(id=1).item_set.get(id=id)}</h1>")
     except:
         return HttpResponse(f"<h1>An item with id: {id} doesn't exist.</h1>")
