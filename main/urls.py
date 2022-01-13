b6783c4fb1dc9e6d8565356e2e4e59703207362d
UnGerardo <gerardojdls@gmail.com>
2022-01-13 13:54:21 -0800

    Created templates folder and html templates, and template inheritance

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from main.views import landingPage
+from main.views import landingPage, home
 
 urlpatterns = [
-    path('<int:id>/', landingPage, name='LandingPage'),
+    path('<int:id>/', landingPage, name='landing page'),
+    path('', home, name='home')
 ]
--- main/views.py
+++ main/views.py
@@ -4,7 +4,11 @@ from .models import ToDoList, Item
 
 # Create your views here.
 def landingPage(request, id):
-    try:
-        return HttpResponse(f"<h1>{ToDoList.objects.get(id=1).item_set.get(id=id)}</h1>")
-    except:
-        return HttpResponse(f"<h1>An item with id: {id} doesn't exist.</h1>")
+    # try:
+    #     return HttpResponse(f"<h1>{ToDoList.objects.get(id=1).item_set.get(id=id)}</h1>")
+    # except:
+    #     return HttpResponse(f"<h1>An item with id: {id} doesn't exist.</h1>")
+    return render(request, 'base.html', {})
+
+def home(request):
+    return render(request, 'home.html', {})
